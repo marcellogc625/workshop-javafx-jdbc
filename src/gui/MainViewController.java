e483279c686ac8a979f104b2ce3c0ca04f997122
Marcello Galvao <marcellogc25@gmail.com>
2023-01-11 20:26:16 -0300

    'MainViewController' created.

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,43 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+public class MainViewController implements Initializable {
+    
+    @FXML
+    private MenuItem menuItemAbout;
+    
+    @FXML
+    private MenuItem menuItemDepartment;
+    
+    @FXML
+    private MenuItem menuItemSeller;
+    
+    @FXML
+    public void onMenuItemAboutAction() {
+	
+    }
+    
+    @FXML
+    public void onMenuItemDepartmentAction() {
+	
+    }
+
+    @FXML
+    public void onMenuItemSellerAction() {
+	
+    }
+    
+    @Override
+    public void initialize(URL uri, ResourceBundle rb) {
+	
+	
+    }
+
+    
+}
