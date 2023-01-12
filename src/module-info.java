4055c2946977da688e435f25cf91d89dbbf0e164
Marcello Galvao <marcellogc25@gmail.com>
2023-01-12 16:08:26 -0300

    Created 'About' view amd added 'Alerts' and 'Constraints' entities.

--- src/module-info.java
+++ src/module-info.java
@@ -4,4 +4,10 @@ module workshopjavajdbc {
 	requires javafx.fxml;
 	
 	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.fxml;
+	opens gui.util to javafx.fxml;
+	
+	exports gui to javafx.fxml;
+	exports gui.util to javafx.fxml;
+	
 }
