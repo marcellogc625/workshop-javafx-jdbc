feb2fea22f0cef6920f7edd0ac7f4918243fcf91
Marcello Galvao <marcellogc25@gmail.com>
2023-01-13 16:09:22 -0300

    'DepartmentService' created, 'DepartmentListController' renamed.

--- src/module-info.java
+++ src/module-info.java
@@ -6,8 +6,12 @@ module workshopjavajdbc {
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
 	opens gui.util to javafx.fxml;
+	opens model.entities to javafx.base, javafx.fxml;
+	opens model.services to javafx.base, javafx.fxml;
 	
 	exports gui to javafx.fxml;
 	exports gui.util to javafx.fxml;
+	exports model.entities to javafx.fxml;
+	exports model.services to javafx.fxml;
 	
 }
