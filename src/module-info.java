a9e0017ee35aedb42a6e8195330f0bebfee5a841
Marcello Galvao <marcellogc25@gmail.com>
2023-01-16 15:09:02 -0300

    Adding database access.

--- src/module-info.java
+++ src/module-info.java
@@ -2,6 +2,7 @@ module workshopjavajdbc {
 	requires javafx.controls;
 	requires javafx.graphics;
 	requires javafx.fxml;
+	requires java.sql;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	opens gui to javafx.fxml;
